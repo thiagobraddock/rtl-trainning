1e94b6bf8e2db7c4b997758bb85714ef5e0eb669
Thiago "BRADDOCK" Oliveira <braddock01@hotmail.com>
2023-01-09 12:03:29 -0300

    add resolucao final

--- src/components/card/card.test.js
+++ src/components/card/card.test.js
@@ -6,7 +6,9 @@ import {
 } from '@testing-library/react';
 import { MemoryRouter, Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
 import App from '../../App';
+import Card from './card';
 
 const mockData = {
   results: [
@@ -44,8 +46,78 @@ async function mockFetch() {
 }
 
 describe('<card />', () => {
-  test.todo('should fetch to be called once');
-  test.todo('should fetch to be called after render Card');
-  test.todo('should render "Morris Henderson, age 33"');
-  test.todo('should render About page after click menu link');
+  // test.todo('should fetch to be called once');
+  // test.todo('should fetch to be called after render Card');
+  // test.todo('should render "Morris Henderson, age 33"');
+  // test.todo('should render About page after click menu link');
+
+  test('should fetch to be called once', async () => {
+    jest.spyOn(global, 'fetch').mockImplementation(mockFetch);
+    const history = createMemoryHistory();
+    await act(async () => {
+      render(
+        <Router history={ history }>
+          <App />
+        </Router>,
+      );
+    });
+
+    expect(global.fetch).toHaveBeenCalled();
+    // const promise = Promise.resolve();
+
+    // await act(async () => {
+    //   await promise;
+    // });
+    expect(history.location.pathname).toBe('/');
+  });
+
+  test('should fetch to be called after render Card', async () => {
+    jest.spyOn(global, 'fetch').mockImplementation(mockFetch);
+
+    render(<MemoryRouter initialEntries={ ['/'] }><App /></MemoryRouter>);
+
+    waitForElementToBeRemoved(() => screen.getByText(/loading/i));
+
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+  });
+
+  test('should render "Morris Henderson, age 33"', async () => {
+    jest.spyOn(global, 'fetch').mockImplementation(mockFetch);
+
+    render(<MemoryRouter><App /></MemoryRouter>);
+
+    const headingName = await screen.findByRole('heading', {
+      name: 'Morris Henderson, age 33',
+    });
+
+    expect(headingName).toBeInTheDocument();
+  });
+
+  test('should render About page after click menu link', async () => {
+    jest.spyOn(global, 'fetch').mockImplementation(mockFetch);
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+
+    // waitForElementToBeRemoved(() => screen.getByText(/loading/i));
+
+    // const linkAbout = await screen.findByRole('link', { name: /about/i });
+
+    // userEvent.click(linkAbout);
+
+    act(() => {
+      history.push('/about');
+    });
+
+    // expect(await screen.findByText(/page/i));
+    expect(history.location.pathname).toBe('/about');
+  });
 });
+
+// global.fetch = jest.fn(async () => ({
+//   ok: true,
+//   json: async () => mockData,
+// }));
